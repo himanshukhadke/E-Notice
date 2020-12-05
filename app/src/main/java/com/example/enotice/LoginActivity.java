d679eb528c7f07f9a4e0fac7c82aebb0f8c4a60d
Himanshu Khadke <khadkeh754@gmail.com>
2020-12-05 16:31:56 +0530

    Added Bottom Navigation bar and toolbar with menu

--- app/src/main/java/com/example/enotice/LoginActivity.java
+++ app/src/main/java/com/example/enotice/LoginActivity.java
@@ -1,8 +1,8 @@
 package com.example.enotice;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText pass;
     private Button login_btn;
     private FirebaseAuth mAuth;
+    private ProgressDialog mLoginProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +29,21 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         id = findViewById(R.id.etxt_login_collegeID);
         pass = findViewById(R.id.etxt_login_password);
-//        System.out.println(college_id+" and "+password);
+        mLoginProgress = new ProgressDialog(this);
         login_btn = findViewById(R.id.btn_login_login);
         login_btn.setOnClickListener(v -> {
-            String college_id=id.getText().toString();
-            String password=pass.getText().toString();
-            startLoginProcess(college_id, password);
+            String college_id = id.getText().toString();
+            String password = pass.getText().toString();
+            if (college_id.equals("") || password.equals("")) {
+                Toast.makeText(LoginActivity.this, "Please enter login details...", Toast.LENGTH_SHORT).show();
+            } else {
+                mLoginProgress.setTitle("Logging in...");
+                mLoginProgress.setMessage("Please wait while we check your credentials");
+                mLoginProgress.setCanceledOnTouchOutside(false);
+                mLoginProgress.show();
+                startLoginProcess(college_id, password);
+
+            }
         });
     }
 
@@ -43,10 +53,13 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                             startActivity(new Intent(LoginActivity.this,MainActivity.class));
-                             finish();
+                            mLoginProgress.dismiss();
+                            Intent newintent = new Intent(LoginActivity.this, MainActivity.class);
+                            newintent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); //user  should not go back to startpage on pressing backspace
+                            finish();
                         } else {
-                             Toast.makeText(LoginActivity.this,"Incorrect Login Details...",Toast.LENGTH_SHORT).show();
+                            mLoginProgress.hide();
+                            Toast.makeText(LoginActivity.this, "Incorrect Login Details...", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
--- app/src/main/java/com/example/enotice/MainActivity.java
+++ app/src/main/java/com/example/enotice/MainActivity.java
@@ -1,37 +1,90 @@
 package com.example.enotice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Button logout;
+    private BottomNavigationView main_bottom_nav;
+    private Toolbar main_toolbar;
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.item_notices:
+                    selectedFragment = new NoticeFragment();
+                    break;
+                case R.id.item_news:
+                    selectedFragment = new NewsFragment();
+                    break;
+                case R.id.item_myprofile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-        logout=findViewById(R.id.btn_main_logout);
-        logout.setOnClickListener(v->{
-            FirebaseAuth.getInstance().signOut();
-            Intent start_intent=new Intent(MainActivity.this,StartActivity.class);
-            startActivity(start_intent);
-            finish();
-        });
+        main_toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(main_toolbar);
+        getSupportActionBar().setTitle("E-Notice");
+        main_bottom_nav = findViewById(R.id.bottom_navigation);
+        main_bottom_nav.setOnNavigationItemSelectedListener(navListner);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, new NoticeFragment()).commit();// By default it shows Notice fragment
     }
+
     public void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser==null){
-            Intent start_intent=new Intent(MainActivity.this,StartActivity.class);
-            startActivity(start_intent);
-            finish();
+        if (currentUser == null) {
+            sendToStart();
         }
+    }
+
+    private void sendToStart() {
+        Intent start_intent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(start_intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_logout:
+                FirebaseAuth.getInstance().signOut();
+                sendToStart();
+                break;
+            case R.id.item_settings:
+                break;
+        }
+        return true;
     }
 }
